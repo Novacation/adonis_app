655f48eb405e2fbf47c866869125b6a511b386db
Celestino <andersoncelestino.n@gmail.com>
2021-12-20 09:47:43 -0300

    Update DashboardController.ts

--- app/Controllers/Http/DashboardController.ts
+++ app/Controllers/Http/DashboardController.ts
@@ -7,7 +7,9 @@ export default class DashboardController {
 
       return response.status(200).json(user);
     } catch (error) {
-      return response.status(401).send('<h1>Register page</h1>');
+      return response
+        .status(401)
+        .send("<h1 style='text-align: center; margin:'center'>Register Page</h1>");
     }
   }
 }
